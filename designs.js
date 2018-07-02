e6281b880c257698130739dc23b795e96305700e
OkohBenson <bensonokohotafu@outlook.com>
2018-07-02 15:50:49 +0800

    you missed a semicolon

--- designs.js
+++ designs.js
@@ -26,11 +26,11 @@ function makeGrid() {
     clickedCell = canvas.find('td');
 
     // When td is clicked, change the color of td
-    clickedCell.click(function () {
+    clickedCell.click(function() {
         // Select color input
         colorPicked = $('#colorPicker').val();
         if ($(this).attr('bgcolor')) {
-            $(this).removeAttr('bgcolor')
+            $(this).removeAttr('bgcolor');
         } else {
             $(this).attr('bgcolor', colorPicked);
         }
@@ -38,9 +38,9 @@ function makeGrid() {
 }
 
 // When the size is submitted, call makeGrid()
-$(document).ready(function () {
+$(document).ready(function() {
     var createGrid = $('input[type="submit"]');
-    createGrid.click(function (event) {
+    createGrid.click(function(event) {
         event.preventDefault();
         makeGrid();
     });
